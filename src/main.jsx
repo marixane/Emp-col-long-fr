26c939316a8663aa280438feb05c08d7f7dbc06d
marixane <prof.marwane@gmail.com>
2026-06-29 19:46:37 +0100

    Import PDF exercise points format

--- src/main.jsx
+++ src/main.jsx
@@ -44,6 +44,7 @@ import './export-pdf-clear.js';
 import './discreet-exercise-points.css';
 import './discreet-duration-control.css';
 import './fix-points-parentheses.js';
+import './pdf-exercise-points-clean.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
